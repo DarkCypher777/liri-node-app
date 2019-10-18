77fd8a43a41eff9668213caea70c36ba26b200bf
Ben Diamond <54002015+DarkCypher777@users.noreply.github.com>
2019-10-18 13:02:49 -0400

    updated files

--- liri.js
+++ liri.js
@@ -1,13 +1,45 @@
-// check activity 17
+require("dotenv").config();
+
+var Spotify = require("node-spotify-api");
+
+var keys = require("./keys.js");
+
 var axios = require("axios");
+
+var moment = require("moment");
+
+var fs = require("fs");
+
+var spotify = new Spotify(keys.spotify);
+
+
+// 9. Make it so liri.js can take in one of the following commands:
+
+//     `concert-this`
+
+//     `spotify-this-song`
+
+//     `movie-this`
+
+//     `do-what-it-says`
+
+var getArtistName = function(artist) {
+  return artist.name;
+};
+
+var getArtistName = 
+
+  // check activity 17, 23, 
+  // Format the address using process.argv
+  // var address = process.argv.slice(2).join(" ");
 
 
 // Then run a request with axios to the OMDB API with the movie specified
 axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log("The movie's rating is: " + response.data.imdbRating);
-  })
-  .catch(function(error) {
+    function (response) {
+      console.log("The movie's rating is: " + response.data.imdbRating);
+    })
+  .catch(function (error) {
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
@@ -29,14 +61,24 @@ axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=tr
   });
 
 
+// Replace with your mapquest consumer API key
+// var options = {
+//   provider: "mapquest",
+//   apiKey: "YOUR-MAPQUEST-API-CONSUMER-KEY"
+// };
 
-    /* Load the HTTP library */
-    var http = require("http");
+// var geocoder = NodeGeocoder(options);
 
-    /* Create an HTTP server to handle responses */
-  
-    http.createServer(function(request, response) {
-      response.writeHead(200, {"Content-Type": "text/plain"});
-      response.write("Hello World");
-      response.end();
-    }).listen(8888);
+// Format the address using process.argv
+// var address = process.argv.slice(2).join(" ");
+
+// Log the address we built
+// console.log("Searching for " + address);
+
+// Then use the geocoder object to search the address
+// geocoder.geocode(address, function(err, data) {
+
+// Then console log the result and stringify it.
+// Note the argument of "2" being included in the JSON stringify. This makes the JSON output pretty.
+// See link here: http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+console.log(JSON.stringify(data, null, 2));
